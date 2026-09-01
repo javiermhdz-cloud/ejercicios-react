934cd74105d2219132b328f4aef7914251f40035
javiermhdz-cloud <javier.macossay.hdz@gmail.com>
2026-09-01 12:42:20 -0600

    fix: add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ejercicio-react/",
+  base: "https://github.com/javiermhdz-cloud/ejercicios-react",
   plugins: [react()],
 })
